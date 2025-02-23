01423d2fb93806517393d1e33af5bf98f9488909
Nishant Argade <nishant@kitchenspurs.in>
2025-02-24 01:24:40 +0530

    auth

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return 'Dashboard';
+        $user = Auth::user();
+
+        if (! $user) {
+            return response()->json([
+                'message' => 'User not authenticated',
+            ], 401);
+        }
+
+        return ['user' => $user];
     }
 }
